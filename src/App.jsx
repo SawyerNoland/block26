c2af59cb27d514d82c2d823b6a9145157c96de52
sawyerno <sawyernoland2005@gmail.com>
2023-09-11 20:11:21 -0400

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
 import { useState, useEffect } from 'react'
 import './App.css'
-import ContactList from './components/contactList'
+import ContactList from './component/ContactList';
+import ContactRow from './component/ContactRow';
+
+// const [contacts, setContacts] = useState(dummyContacts)
+
+//we want to create a new fucntion that populates the selected info (onClick()) into a new div that displays only the selected individuals data
 
 function App() {
-  const [count, setCount] = useState(0)
-  const [contacts, setContacts] = useState(dummyContacts)
+  const [contacts, setContacts] = useState([])
+  const [selectedContactId, setSelectedContactId] = useState(null)
+  const  featuredContact = contacts.find((contact) => contact.id === selectedContactId)
 
+useEffect(() => {
+    async function fetchContacts(){
+        try {
+            const response = await fetch("https://fsa-jsonplaceholder-69b5c48f1259.herokuapp.com/users");
+            const result = await response.json();
+            setContacts(result)
+        } catch (error) {
+            console.log(error);
+        }
+    }
+    fetchContacts()
+}, []);
+  
   return (
     <>
-      <ContactList/>
+    <div className="table-container">
+      <ContactList contacts={contacts} setSelectedContactId={setSelectedContactId}/>
+    </div>
+    {selectedContactId && (
+      <div className='selected-info'>{featuredContact.name} {featuredContact.email} {featuredContact.phone}</div>
+    )}
+ 
     </>
-  )
+  );
 }
+
 
 export default App
